1215e3bfbcbdad79f61b88194019957c3a716efb
Jonmichael Chambers <jon@tyrbo.net>
2016-06-06 19:56:21 -0600

    Eh, forget it. We don't need to exclude anything here.

--- app/controllers/targets_controller.rb
+++ app/controllers/targets_controller.rb
@@ -1,17 +1,11 @@
 class TargetsController < ApplicationController
   def index
-    render json: targets, each_serializer: MiniTargetSerializer
+    render json: Target.with_available_state.order(name: :asc), each_serializer: MiniTargetSerializer
   end
 
   def show
     target = Target.eager_load(:category, :constellation, :knowledges, :results).find(params[:id])
 
     render json: target
-  end
-
-  private
-
-  def targets
-    Target.joins(:results).where.not(results: { id: nil }).uniq
   end
 end
