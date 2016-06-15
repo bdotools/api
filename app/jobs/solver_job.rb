d0afac1403f2f328631fc520b20bd1d6db0d828a
Jonmichael Chambers <jon@tyrbo.net>
2016-06-15 04:40:43 -0600

    Oops, we should be using id always

--- app/jobs/solver_job.rb
+++ app/jobs/solver_job.rb
@@ -1,8 +1,8 @@
 class SolverJob < ApplicationJob
   queue_as :default
 
-  def perform(name, interest, favor)
-    pid = Process.spawn "bin/solver SolveFast \"#{name}\" #{interest} #{favor} 6"
+  def perform(id, interest, favor)
+    pid = Process.spawn "bin/solver SolveFast \"#{id}\" #{interest} #{favor} 6"
     Process.wait pid
   end
 end
