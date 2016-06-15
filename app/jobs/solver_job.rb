9f85ca0d1cf4d08966165446600cdbfe02a533d2
Jonmichael Chambers <jon@tyrbo.net>
2016-06-15 03:01:22 -0600

    Oops, fix bug.

--- app/jobs/solver_job.rb
+++ app/jobs/solver_job.rb
@@ -2,7 +2,7 @@ class SolverJob < ApplicationJob
   queue_as :default
 
   def perform(name, interest, favor)
-    pid = Process.spawn "bin/solver SolveFast \"#{target.name}\" #{interest} #{favor} 6"
+    pid = Process.spawn "bin/solver SolveFast \"#{name}\" #{interest} #{favor} 6"
     Process.wait pid
   end
 end
