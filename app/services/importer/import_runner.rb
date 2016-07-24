a8fb56fc8c46df61e56fb998a542193617ecfd11
Jonmichael Chambers <jon@tyrbo.net>
2016-07-24 19:21:35 +0000

    Fixes naming

--- app/services/importer/import_runner.rb
+++ app/services/importer/import_runner.rb
@@ -8,7 +8,7 @@ module Importer
       Globalize.with_locale(:ko) do
         ImportCard.new(path: "#{@path}/datasheets_Card.csv").run!
         ImportPersonality.new(path: "#{@path}/datasheets_Personality.csv").run!
-        ImportTarget.new(path: "#{@path}/datasheets_character.csv").run!
+        ImportTarget.new(path: "#{@path}/datasheets_Character.csv").run!
         ImportTheme.new(path: "#{@path}/datasheets_Theme.csv").run!
         ImportZodiac.new(path: "#{@path}/datasheets_ZodiacSign.csv").run!
         ImportZodiacIndex.new(path: "#{@path}/datasheets_ZodiacSignIndex.csv").run!
