1d051b4f7a149234a68d39f6b5505769448d90d4
Jonmichael Chambers <jon@tyrbo.net>
2016-07-26 10:40:34 +0000

    Return best result if availabe

--- app/controllers/query_controller.rb
+++ app/controllers/query_controller.rb
@@ -2,9 +2,19 @@ class QueryController < ApplicationController
   FAIL_PARAMS = [1, 3]
 
   def show
-    order = fail_params.include?(params[:g].to_i) ? :asc : :desc
+    result = Result.find_by(
+      target_id: params[:t],
+      goal: params[:g],
+      goal_param: params[:gp],
+      target_favor: params[:f],
+      target_interest: params[:i]
+    )
 
-    result = BestCombo.new(id: params[:t], order: order, interest: params[:i], favor: params[:f])
+    if !result
+      order = fail_params.include?(params[:g].to_i) ? :asc : :desc
+      result = BestCombo.new(id: params[:t], order: order, interest: params[:i], favor: params[:f])
+    end
+
     render json: result
   end
 
--- app/services/importer/import_zodiac_index.rb
+++ app/services/importer/import_zodiac_index.rb
@@ -5,7 +5,7 @@ module Importer
     model ConstellationOrder
 
     column :indices, as: [/\AIndex\d{1}+\z/], to: ->(order, model) do
-      return if order.nil? || order == "<null>"
+      return if (order.nil? || order == "<null>")
       model.slot_order << order
     end
     column :constellation_id, as: ["ZodiacSignKey"], required: true
